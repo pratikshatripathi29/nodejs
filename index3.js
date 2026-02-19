7c5c29485f451a0e393798446fdc87377e36dcdc
pratikshatripathi29 <tripathipratiksha18@gmail.com>
2026-02-19 11:37:43 +0530

    updated node files

--- index3.js
+++ index3.js
@@ -1,7 +1,15 @@
 import express from 'express'
 const app=express()
 app.listen(8080,()=>console.log("Server started"))
+app.get("/",(req,res)=>{
+    console.log("req.url")
+    res.send("response from server for /")
+})
 app.get("/home",(req,res)=>{
     console.log("req.url")
-    res.send("response from server for / home")
+    res.send("response from server for /home")
+})
+app.get("/home/page",(req,res)=>{
+    console.log("req.url")
+    res.send("response from server for /home/page")
 })
