140f45dd7cb7f1fb40e78547263e03b097f0ac55
pratikshatripathi29 <tripathipratiksha18@gmail.com>
2026-02-19 11:34:25 +0530

    updated node files

--- index1.js
+++ index1.js
@@ -1,2 +1,8 @@
-console.log("Hello World")
+import express from "express";
+const  app=express ()
+app.listen(8080)
+
+
+
+// console.log("Hello World")
 // npm init -y
---
+++ index3.js
@@ -0,0 +1,7 @@
+import express from 'express'
+const app=express()
+app.listen(8080,()=>console.log("Server started"))
+app.get("/home",(req,res)=>{
+    console.log("req.url")
+    res.send("response from server for / home")
+})
