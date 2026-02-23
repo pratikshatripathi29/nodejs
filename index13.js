48feac573505807c3343259f02276736fc4ed161
pratikshatripathi29 <tripathipratiksha18@gmail.com>
2026-02-23 10:25:28 +0530

    new files

---
+++ index13.js
@@ -0,0 +1,17 @@
+// import express from "express"
+// const app = express();
+// app.listen(8080, () => console.log("Server started"));
+
+// const Router = express.Router();
+
+
+// Router.get("/", (req, res) => {
+//     res.send("Hello");
+// });
+
+// Router.post("/", (req, res) => {
+//     res.send("This is post request");
+// });
+
+// app.use("/api/users", Router);
+
